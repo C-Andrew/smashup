c1f08b04732c48f24c0ab31ff569d93f19b87f03
Andrew <Andrew@v1020-wn-219-26.campus-dynamic.uwaterloo.ca>
2015-10-22 23:33:03 -0400

    No assets but working dealer

---
+++ factions.js
@@ -0,0 +1,93 @@
+
+var available_factions = [];
+
+var dealt_factions = [];
+
+var expansions = {
+    "gen1" : {
+      "name" : "Core",
+      "gen" : "Gen1",
+      "list" : ["Robots", "Zombies", "Pirates", "Aliens", "Dinosaurs", "Ninjas", "Tricksters", "Wizards", "Geeks"]
+    },
+    "gen2" : {
+      "name" : "Awesome Level 9000",
+      "gen" : "Gen2",
+      "list" : ["Bear Cavalry", "Ghosts", "Killer Plants", "Steampunks"]
+    },
+    "gen3" : {
+      "name" : "Science Fiction Double Feature",
+      "gen" : "Gen3",
+      "list" : ["Cyborg Apes", "Shapeshifters", "Super Spies", "Time Travelers"]
+    },
+    "gen4" : {
+      "name" : "Monster Smash",
+      "gen" : "Gen4",
+      "list" : ["Giant Ants", "Mad Scientists", "Vampires", "Werewolves "]
+    },
+    "gen5" : {
+      "name" : "Pretty Pretty Smash Up",
+      "gen" : "Gen5",
+      "list" : ["Fairies", "Kitty Cats", "Mythic Horses", "Princesses "]
+    }
+};
+
+/*
+    Core 
+    Aliens - Return minions back to their owners' hands and gain victory points without breaking bases.
+    Dinosaurs - High-powered minions break bases quickly.
+    Ninjas - Destroy minions and sneak onto bases at the last second.
+    Pirates - Move around from base to base and destroy weak minions.
+    Robots - Play lots of weak minions at once.
+    Tricksters - Force other players to discard and make bases undesirable to play on.
+    Wizards - Draw quickly, and play several cards per turn.
+    Zombies - Play minions from the discard pile.
+    
+    Awesome Level 9000
+    Bear Cavalry - Move other players' minions and destroy them.
+    Ghosts - Benefit from discarding and having 2 or fewer cards in their hand.
+    Killer Plants - Search your deck for weak minions, play extra minions, and setup multi-turn abilities.
+    Steampunks - Powerful base actions that can be played from the discard pile.
+
+    The Obligatory Cthulhu 
+    Elder Things - Give Madness to other players, and have strong minions that are difficult to play.
+    Innsmouth - Lots of power 2 minions that work together well.
+    Minions of Cthulhu a.k.a. Cthulhu Cultists - Play lots of actions, recover cards from the discard pile, and slow the game down all the while gaining Madness.
+    Miskatonic University - Gain, use, and lose Madness to accomplish a little of everything.
+    
+    Science Fiction Double Feature
+    Cyborg Apes - Play actions on minions to power them up.
+    Shapeshifters - Copy other players' power and abilities.
+    Super Spies - Sabotage other players' hands and decks, and play specials while bases are scoring.
+    Time Travelers - Replay actions and minions.
+
+    Monster Smash
+    Giant Ants - Play and move +1 power counters on your minions.
+    Mad Scientists - Play and manipulate +1 power counters on your minions.
+    Vampires - Destroy minions to gain power.
+    Werewolves - Gain power on your turn and destroy minions.
+
+    The Big Geeky Box
+    Geeks - Interrupt other players' turns to disrupt their plans.
+
+    Pretty Pretty Smash Up
+    Fairies - Choose between effects and manipulate actions played on minions.
+    Kitty Cats - Temporarily take control of other player's minions, and destroy minions you control.
+    Mythic Horses - Benefit from having lots of minions together.
+    Princesses - A few strong minions with powerful abilities.
+
+    Smash Up: Munchkin
+    Clerics - return cards from the discard pile and prevent them from going to the discard pile
+    Dwarves - benefit from treasures played on their minions
+    Elves - help other factions to help self
+    Halflings - playing lots of extra minions, often negating their effects to play them
+    Mages -  discarding cards for destroying and other effects
+    Orcs - huge brute horse against your opponents
+    Thieves - steal things sell treasures for VP
+    Warriors - playing more monsters on bases and destroying monsters on bases
+
+    Smash up: It's Your Fault 
+    Sharks
+    Superheros
+    Greek Myth
+    Dragons
+*/
--- js/index.js
+++ js/index.js
@@ -1,12 +1,6 @@
 console.log("hello world!");
 
-var local = {
-  "factions" : {
-    "gen1" : ["Robots", "Zombies", "Pirates", "Aliens", "Dinosaurs", "Ninjas", "Tricksters", "Wizards"],
-    "gen2" : ["Bear Cavalry", "Ghosts", "Killer Plants", "Steampunks"],
-    "gen3" : ["Giant Ants", "Mad Scientists", "Vampires"]
-  }
-};
+
 
 $( document.body ).on( 'click', '.dropdown-menu li', function( event ) {
 
@@ -22,21 +16,129 @@ $( document.body ).on( 'click', '.dropdown-menu li', function( event ) {
 });
 
 $(document).ready(function(){
+    // For every entry in factions, create a tab.
+    $.each(expansions, function(gen, val) {
+      // Add Expansion to tab
+      var genItem = "<li><a href=\"#" + val.gen +  "\">" + val.name + "</a></li>";
+      $("#faction-list").append(genItem);
+
+      // Add tab-pane for that Expansion
+      var pane = "<div id=\"" + val.gen + "\" class=\"tab-pane fade row\"></div>";
+      $(".tab-content").append(pane);
+
+
+      // For every faction in expansion
+      $.each(val.list, function(){
+        var faction_name = this.valueOf();
+        var faction_id = faction_name.replace(/ /g,'');
+        var $div = $("<div>", {
+          id: faction_id, 
+          class: "well well-sm col-sm-3",
+          html: faction_name
+        });
+
+        $div.click(function(){
+          console.log("clicked");
+          var hasFaction = available_factions.indexOf(faction_id);
+          if(hasFaction > -1){
+            available_factions.splice(hasFaction, 1);
+          } else {
+            available_factions.push(faction_id);
+          }
+        });
+
+        $("#" + val.gen).append($div);
+        available_factions.push(faction_id);
+      })  
+    });
+
+    $('#faction-list a[href="#Gen1"]').tab('show');
+
     $(".nav-tabs a").click(function(){
         $(this).tab('show');
+        console.log("hello");
     });
     $('.nav-tabs a').on('shown.bs.tab', function(event){
-        var x = $(event.target).text();         // active tab
-        var y = $(event.relatedTarget).text();  // previous tab
-        $(".act span").text(x);
-        $(".prev span").text(y);
+        // var x = $(event.target).text();         // active tab
+        // var y = $(event.relatedTarget).text();  // previous tab
+        // $(".act span").text(x);
+        // $(".prev span").text(y);
     });
+
+    $("#deal-btn").click(function(){ 
+      // Deep copy array
+      dealt_factions = available_factions.slice();
+
+      var numOfPlayers = $("#numOfPlayers").val();
+      var numOfFactions = $("#numOfFactions").val();
+
+      // Clear all dealt factions
+      $(".panel-body > p").remove()
+
+      // Deal factions to all players
+      for(var x = 1; x <= numOfPlayers; x++){
+        console.log(dealt_factions);
+        console.log("Player " + x + ":");
+        for(var y = 0; y < numOfFactions; y++){
+          var index = getRandomInt(0, dealt_factions.length-1);
+          var hand = "hand" + x;
+          var $p = $("<p>", {
+            html: dealt_factions[index]
+          });
+
+          // Add faction to player's hand
+          $("#" + hand).append($p);
+          console.log(dealt_factions[index]);
+          // Remove faction from factions
+          dealt_factions.splice(index, 1);
+        }
+      }
+
+    }); 
+
+    $( "#numOfPlayers" ).change(function() {
+      switch(parseInt($("#numOfPlayers").val())){
+        case 2:
+          $('#player2').removeClass('hide');
+          $('#player3').addClass('hide');
+          $('#player4').addClass('hide');
+          $('#player5').addClass('hide');
+          $('#player6').addClass('hide');    
+          break;    
+        case 3:
+          $('#player3').removeClass('hide');
+          $('#player4').addClass('hide');
+          $('#player5').addClass('hide');
+          $('#player6').addClass('hide');
+          break;
+        case 4:
+          $('#player4').removeClass('hide');
+          $('#player5').addClass('hide');
+          $('#player6').addClass('hide');
+          break;
+        case 5:
+          $('#player5').removeClass('hide');
+          $('#player6').addClass('hide');
+          break;
+        case 6:
+          $('#player6').removeClass('hide');
+          break;
+        default: 
+          break;
+      }
+
+      console.log($("#numOfPlayers").val());
+    });
+
 });
  
-function init(){
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-init();
+
+
 
 
 
